639bf5fe28d69da1c4295470a43b3be8e62ad5c0
illbloo <illbloo@morefunweb.co>
2025-08-12 10:08:23 -0700

    (wip) opencode sound effects too

---
+++ .opencode/plugin/sfx.ts
@@ -0,0 +1,29 @@
+import type { Plugin } from "@opencode-ai/plugin"
+
+type SoundConfig = {
+  [key: string]: {
+    path: string;
+  }
+}
+
+const soundConfig: SoundConfig = {
+  "session.idle": {
+    "path": "~/.claude/sounds/mew.wav"
+  },
+}
+
+export const MyPlugin: Plugin = async ({ app, client, $ }) => {
+  const playSound = async (path: string) => {
+    await $`afplay ${path} &`.quiet();
+  }
+
+  return {
+    event: async ({ event }) => {
+      for (const [key, value] of Object.entries(soundConfig)) {
+        if (event.type === key) {
+          void playSound(value.path);
+        }
+      }
+    }
+  }
+}
